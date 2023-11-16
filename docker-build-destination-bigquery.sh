80f44c49a591a39c0e0fe1d2ed9d572a2f6bcb39
Daniel <bboyzepot@gmail.com>
2023-11-16 17:41:59 +0900

    update

--- docker-build-destination-bigquery.sh
+++ docker-build-destination-bigquery.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
 
 echo "[INFO] Starting Build of the BigQuery Connector..."
-./gradlew :airbyte-integrations:connectors:destination-bigquery:build
+#./gradlew :airbyte-integrations:connectors:destination-bigquery:build
 echo "[SUCCESS] BigQuery Connector Build Completed."
 
 echo "[INFO] Starting Docker Image Build..."
-DOCKER_IMAGE="quotabook/airbyte-connector-destination-bigquery:2.0.20-1"
-docker build ./airbyte-integrations/connectors/destination-bigquery -t "${DOCKER_IMAGE}"
-echo "[SUCCESS] Docker Image Build Completed (${DOCKER_IMAGE})."
+DOCKER_IMAGE="quotabook/airbyte-connector-destination-bigquery:2.3.14"
 
 echo "[Action Required] Trying to Log in to DockerHub as 'quotabook' (Please use DockerHub PAT for authentication)..."
 docker login --username=quotabook
 echo "[SUCCESS] DockerHub Login Successful."
 
-echo "[INFO] Starting Image Push..."
-docker push "${DOCKER_IMAGE}"
+docker buildx build --platform linux/amd64,linux/arm64 ./airbyte-integrations/connectors/destination-bigquery -t "${DOCKER_IMAGE}" --push
+echo "[SUCCESS] Docker Image Build Completed (${DOCKER_IMAGE})."
+
 echo "Image Upload Successful (${DOCKER_IMAGE}) 🚀"
 docker logout
 echo "[INFO] Logged out from DockerHub."
